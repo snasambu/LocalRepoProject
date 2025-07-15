85daa6355b9cf20b6297f8efcb34b97bd2ffd156
Sophy <snasambu40@gmail.com>
2025-07-15 21:21:32 +0300

    Fix: Clean version of pattern_drawing.py with while and nested for loop

--- Desktop/alx_be_python/control-flow/pattern_drawing.py
+++ Desktop/alx_be_python/control-flow/pattern_drawing.py
@@ -4,7 +4,7 @@ size = int(input("Enter the size of the pattern: "))
 
 row = 0
 while row < size:
-    for _ in range(size):
+    for col in range(size):
         print("*", end="")
-    print()  # Move to the next line after each row
+    print()
     row += 1
